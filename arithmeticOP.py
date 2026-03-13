4da85c57890246feef5d8835177cdffd9e8a3a1c
Biswajit Bhunia <44657622+biswajitbhunia@users.noreply.github.com>
2026-03-13 21:51:51 +0530

    d

---
+++ arithmeticOP.py
@@ -0,0 +1,13 @@
+##Operators in Python
+## Arithmetic Operators:
+## Designed by Biswajit Bhunia
+x = 10
+y = 3
+
+print(x + y)   # 13
+print(x - y)   # 7
+print(x * y)   # 30
+print(x / y)   # 3.3333333333333335 (Normal division: Quotient)
+print(x // y)  # 3 (Floor division: how many full times does 3 go into 10?)
+print(x % y)   # 1 (Modulo: 10 divided by 3 is 9, with 1 left over)
+print(x ** y)  # 1000 (10 to the power of 3)
---
+++ membershipOP.py
@@ -0,0 +1,11 @@
+backpack = ["laptop", "notebook", "pen"]
+
+print("pen" in backpack)      # True
+print("snacks" in backpack)   # False
+print("snacks" not in backpack) # True
+s="hello world"
+print("hello" in s)            # True
+print("world" in s)            # True
+print("hi" in s)               # False
+print("hi" not in s)           # True
+print("Hello" in s)            # False (case-sensitive)
